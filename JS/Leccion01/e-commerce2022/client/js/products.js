ddaae2aeb633841a8d44e776fc68e7430988618c
DeisyOrtega <et5ortegadeisy@gmail.com>
2023-08-21 19:51:53 -0300

    js-1.3
    subo archivo index.js y products.js

---
+++ JS/Leccion01/e-commerce2022/client/js/products.js
@@ -0,0 +1,47 @@
+const productos = [
+    {
+        id: 1,
+        productName: "Kiwi",
+        price: 480,
+        quanty: 1,
+        img: "client/media/kiwi.jpg"
+    },
+    {
+        id: 2,
+        productName: "Fresas",
+        price: 980,
+        quanty: 1,
+        img: "client/media/fresas.jpg"
+        
+    },
+    {
+        id: 3,
+        productName: "Frutas",
+        price: 1180,
+        quanty: 1,
+        img:  "client/media/frutas.jpg"
+    },
+    {
+        id: 4,
+        productName: "Melon",
+        price: 2080,
+        quanty: 1,
+        img:"client/media/melon.jpg"
+
+    },
+    {
+        id: 5,
+        productName: "Mina",
+        price: 150,
+        quanty: 1,
+        img:"client/media/mina.jpg"
+    },
+    {
+        id: 6,
+        productName: "Tobi",
+        price: 850,
+        quanty: 1,
+        img:"client/media/tobi.jpg"
+    },
+
+];
