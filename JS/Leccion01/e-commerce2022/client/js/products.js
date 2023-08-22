0fcb6fccda4322ba04f30d621c1148bdd5b3381c
YamilaCarlucci <yamila.carlucci@gmail.com>
2023-08-22 15:07:42 -0300

    Fix JS leccion01

--- JS/Leccion01/e-commerce2022/client/js/products.js
+++ JS/Leccion01/e-commerce2022/client/js/products.js
@@ -1,47 +1,40 @@
 const productos = [
     {
         id: 1,
-        productName: "Kiwi",
+        productName: "Banana",
         price: 480,
         quanty: 1,
-        img: "client/media/kiwi.jpg"
+        img: "/e-commerce2022/client/media/banana.jpeg"
     },
     {
         id: 2,
-        productName: "Fresas",
+        productName: "Enlatados",
         price: 980,
         quanty: 1,
-        img: "client/media/fresas.jpg"
+        img: "/e-commerce2022/client/media/enlatados.jpeg"
         
     },
     {
         id: 3,
-        productName: "Frutas",
-        price: 1180,
+        productName: "Leche",
+        price: 500,
         quanty: 1,
-        img:  "client/media/frutas.jpg"
+        img:  "/e-commerce2022/client/media/leche.jpeg"
     },
     {
         id: 4,
-        productName: "Melon",
-        price: 2080,
+        productName: "Mayonesa",
+        price: 850,
         quanty: 1,
-        img:"client/media/melon.jpg"
+        img:"/e-commerce2022/client/media/mayonesa.jpeg"
 
     },
     {
         id: 5,
-        productName: "Mina",
-        price: 150,
+        productName: "Pollo",
+        price: 2400,
         quanty: 1,
-        img:"client/media/mina.jpg"
+        img:"/e-commerce2022/client/media/pollo.jpeg"
     },
-    {
-        id: 6,
-        productName: "Tobi",
-        price: 850,
-        quanty: 1,
-        img:"client/media/tobi.jpg"
-    },
-
+   
 ];
