da0180d8f7f7f4bf80473e1ce1291133b9f26f67
YamilaCarlucci <yamila.carlucci@gmail.com>
2023-08-28 20:49:48 -0300

    Fix Java

--- Java/Leccion02/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
+++ Java/Leccion02/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
@@ -1,12 +1,13 @@
 package utn.estudiantes;
 
-import ...
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication 
+@SpringBootApplication
 public class EstudiantesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EstudiantesApplication.class, args);
-	
 	}
 
+}
