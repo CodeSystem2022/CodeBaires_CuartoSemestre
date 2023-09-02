c05f0bab902afad0f4fdc9eaa63f6edf1c9c9f9b
MaiaStrusiat <112667066+MaiaStrusiat@users.noreply.github.com>
2023-09-02 20:45:58 +0200

    Subo practica Java

--- Java/Leccion03/estudiantes/src/main/java/utn/estudiantes/modelo/Estudiante.java
+++ Java/Leccion03/estudiantes/src/main/java/utn/estudiantes/modelo/Estudiante.java
@@ -1,4 +1,15 @@
 package utn.estudiantes.modelo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+
 public class Estudiante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idEstudiante;
+
 }
