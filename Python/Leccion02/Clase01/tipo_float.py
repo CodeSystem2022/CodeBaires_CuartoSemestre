edbea31f6cd909b8d80e7827e3a38c84556cf0e6
MaiaStrusiat <112667066+MaiaStrusiat@users.noreply.github.com>
2023-09-03 21:20:47 +0200

    Subo practica Python

--- Python/Leccion02/Clase01/tipo_float.py
+++ Python/Leccion02/Clase01/tipo_float.py
@@ -9,5 +9,16 @@ print(f'a: {a:.2f}')
 
 # Notacion expontecial (valores positivos o negativos)
 a = 3e5
-print(f'a: {a}')
+print(f'a: {a:.2f}')
+
+a = 3e-5
+print(f'a: {a:.5f}')
+
+#Cualquier calculo que incluye un float, todo cambia a float
+
+a=4.0+5
+print(a)
+print(type(a))
+
+
 
