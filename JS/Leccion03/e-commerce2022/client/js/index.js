e3fde5af35c56fa17113ce484e5c779b3380ae77
Marina Falcon <falcon.marina.it@gmail.com>
2023-09-12 01:01:08 -0300

    Js_3.3 video_3
    Configuramos las cantidades de productos

--- JS/Leccion03/e-commerce2022/client/js/index.js
+++ JS/Leccion03/e-commerce2022/client/js/index.js
@@ -16,13 +16,23 @@ productos.forEach((product) =>{
     content.append(buyButton);
 
     buyButton.addEventListener("click", () => {
-        cart.push({
-            id: product.id,
-            productName: product.productName,
-            price: product.price,
-            quantity: product.quantity,
-            img: product.img,
-        })
-        console.log(cart)
-    })
+        const repeat = cart.some((repeatProduct)=> repeatProduct.id === product.id );
+
+       if(repeat){
+            cart.map((prod)=> {
+                if(prod.id === product.id) {
+                    prod.quanty++;
+                }
+            });
+        }else{
+            cart.push({
+                id: product.id,
+                productName: product.productName,
+                price: product.price,
+                quantity: product.quantity,
+                img: product.img,
+            });
+        }
+        
+    });
 });
