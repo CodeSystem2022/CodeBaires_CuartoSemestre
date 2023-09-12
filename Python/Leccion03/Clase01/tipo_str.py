7c888a8736da96985fed82a4d68f44087fa3797f
matiasq3 <qmatias@yahoo.com>
2023-09-12 17:57:59 -0300

    Python - 1.3 Concatenación de String
    python1.3 Concatenación de String

---
+++ Python/Leccion03/Clase01/tipo_str.py
@@ -0,0 +1,9 @@
+# Profundizacion en el tipo String
+# Concatenacion automatica en Python
+
+variable = 'Adios'
+mensaje = ' Hola ' + 'Alumnos'+ variable # concatenamos
+mensaje += ' ,Terminamos'
+print(mensaje)
+
+
