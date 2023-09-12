901ec3a6efbfe2b38154bdc58a2710569fe3a817
DeisyOrtega <et5ortegadeisy@gmail.com>
2023-09-12 07:55:48 -0300

    python-1.2-Colecciones
    Trabajamos con tipo Colecciones(Lista, Tupla y Diccionario)

--- Python/Leccion03/Clase01/tipo_bool.py
+++ Python/Leccion03/Clase01/tipo_bool.py
@@ -17,6 +17,34 @@ valor = 'Hola'
 resultado = bool(valor)
 print(f'valor: {valor}, Resultado: {resultado}')
 
+# tipo de colecciones ->false para colecciones vacias
+# true para todas las demas
+#List
+valor = []
+resultado = bool(valor)
+print(f'valor de una lista vacia: {valor}, Resultado: {resultado}')
+
+valor = [2, 3, 4, 5]
+resultado = bool(valor)
+print(f'valor de una lista con elementos: {valor}, Resultado: {resultado}')
+
+#Tupla
+valor = ()
+resultado = bool(valor)
+print(f'valor de una tupla vacia: {valor}, Resultado: {resultado}')
+
+valor = (5,)
+resultado = bool(valor)
+print(f'valor de una tupla con elementos: {valor}, Resultado: {resultado}')
+#Diccionario
+valor = {}
+resultado = bool(valor)
+print(f'valor de un diccionario vacio: {valor}, Resultado: {resultado}')
+
+valor = {'Nombre':'juan', 'Apellido': 'Perez'}
+resultado = bool(valor)
+print(f'valor de un diccionario con elementos: {valor}, Resultado: {resultado}')
+
 # Sentencias de control con bool
 if (1,):
     print('Regresa verdadero')
