0d8d2b4ad6562255089756d543268c0408993d13
Marina Falcon <falcon.marina.it@gmail.com>
2023-09-12 03:45:55 -0300

    Comenzamos con la capa de presentacion
     Implementación de la interface CommandLineRunner y  Creamos el objeto Logger

--- Java/Leccion04/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
+++ Java/Leccion04/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
@@ -1,13 +1,29 @@
 package utn.estudiantes;
 
+
+import org.slf4j.Logger;
+import org.sl4f4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import utn.estudiantes.servicio.EstudianteServicio;
+
 
 @SpringBootApplication
-public class EstudiantesApplication {
+public class EstudiantesApplication implements CommandLineRunner{
+
+	@Autowired
+	private EstudianteServicio estudianteServicio;
+	private static final Logger logger = LoggerFactory.getLogger(EstudiantesApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(EstudiantesApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
 	}
 
 }
