e335c00544b00a21c6abea8986c24ebf9c3b0080
YamilaCarlucci <yamila.carlucci@gmail.com>
2023-09-12 09:43:04 -0300

    Update EstudiantesApplication.java

--- Java/Leccion04/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
+++ Java/Leccion04/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
@@ -2,7 +2,7 @@ package utn.estudiantes;
 
 
 import org.slf4j.Logger;
-import org.sl4f4j.LoggerFactory;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
