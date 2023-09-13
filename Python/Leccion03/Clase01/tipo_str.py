44d4dab77096f1702d30a8fca632ceefeb0aaab8
Marina Falcon <falcon.marina.it@gmail.com>
2023-09-13 00:17:05 -0300

    subo metodo help()
     Consulta con la clase str (String)

---
+++ Python/Leccion03/Clase01/tipo_str.py
@@ -0,0 +1,14 @@
+# Profundizacion en el tipo String
+# Concatenacion automatica en Python
+
+variable = 'Adios'
+mensaje = ' Hola ' + 'Alumnos'+ variable # concatenamos
+mensaje += ' ,Terminamos'
+print(mensaje)
+
+#Usamosla la clase help para ayuda o documentacion(built-in)
+
+help(str)
+
+
+
