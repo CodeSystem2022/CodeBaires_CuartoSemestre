ba8a85a92b028ee3a01f508db7fa8c82049bf490
Emmanueltoro28 <111504496+Emmanueltoro28@users.noreply.github.com>
2023-09-17 18:57:33 -0300

    subo NODE video 5.2

--- Node Js/01-Hola-Mundo/monohilo.js
+++ Node Js/01-Hola-Mundo/monohilo.js
@@ -1,3 +1,9 @@
 
 console.log('Hola a toda la cohorte 2022') // Ejecutamos desde la terminal, digitando node monohilo
 
+var i = 0;
+setInterval(function() {
+    console.log(i);
+    i++;
+    
+}, 1000);
