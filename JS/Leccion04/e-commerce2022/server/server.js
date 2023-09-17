3884e81165d17e06b82cf014478f2a7d527e7b5e
YamilaCarlucci <yamila.carlucci@gmail.com>
2023-09-17 13:42:48 -0300

    Update server.js

--- JS/Leccion04/e-commerce2022/server/server.js
+++ JS/Leccion04/e-commerce2022/server/server.js
@@ -6,7 +6,7 @@ const path = require("path");
 
 // REPLACE WITH YOUR ACCESS TOKEN AVAILABLE IN: https://developers.mercadopago.com/panel
 mercadopago.configure({
-	access_token: "<ACCESS_TOKEN>6",
+	access_token: "<ACCESS_TOKEN>",
 });
 
 
