9ab5fb5a6d1c10a488b9eb4629bfd37ee9960b3a
DeisyOrtega <et5ortegadeisy@gmail.com>
2023-09-18 08:06:48 -0300

    Node-5.3
    creamos un error  forzado

--- Node Js/01-Hola-Mundo/monohilo.js
+++ Node Js/01-Hola-Mundo/monohilo.js
@@ -5,6 +5,9 @@ var i = 0;
 setInterval(function() {
     console.log(i);
     i++;
-    
+    if(i === 5){
+        console.log('forzamos un error');
+        var a = 3 + z;
+    }
 }, 1000);
 console.log('Segunda instruccion');
