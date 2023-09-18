bc3451295f50c76f15a08b5199e56507156139e0
matiasq3 <qmatias@yahoo.com>
2023-09-18 19:04:07 -0300

    JAVA - 5.2 Aplicación en las teorías
    Modifico el nombre de la clase entidad Estudiante a Estudiantes2022 de EstudianteServicio.java

--- Java/Leccion04/estudiantes/src/main/java/utn/estudiantes/servicio/IEstudianteServicio.java
+++ Java/Leccion04/estudiantes/src/main/java/utn/estudiantes/servicio/IEstudianteServicio.java
@@ -1,14 +1,14 @@
 package utn.estudiantes.servicio;
 
-import utn.estudiantes.modelo.Estudiante;
+import utn.estudiantes.modelo.Estudiantes2022;
 
 import java.util.List;
 
 public interface IEstudianteServicio {  // Definimos metodos de la interface, que luego implementaremos desde la clase EstudianteServicio
-    public List<Estudiante> listarEstudiantes();
-    public Estudiante buscarEstudiantePorId(Integer idEstudiante);
-    public void guardarEstudiante(Estudiante estudiante);  //objeto estudiante de tipo Estudiante
-    public void eliminarEstudiante(Estudiante estudainte);
+    public List<Estudiantes2022> listarEstudiantes();
+    public Estudiantes2022 buscarEstudiantePorId(Integer idEstudiante);
+    public void guardarEstudiante(Estudiantes2022 estudiante);  //objeto estudiante de tipo Estudiantes2022
+    public void eliminarEstudiante(Estudiantes2022 estudainte);
 
 
 }
