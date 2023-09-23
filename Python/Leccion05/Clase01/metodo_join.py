de668173e6c62f6fd2adfe9498e3c222d4558982
Marina Falcon <falcon.marina.it@gmail.com>
2023-09-23 02:00:33 -0300

    Metodo join para colecciones a cadena parte 1

---
+++ Python/Leccion05/Clase01/metodo_join.py
@@ -0,0 +1,9 @@
+#Metodo join para colecciones a cadenas
+
+#help(str.join)
+
+
+tupla_str = ('Hola','alumnos','Tecnicatura','Universitaria')
+mensaje = '-'.join(tupla_str)
+print(f'Mensaje: {mensaje}')
+
