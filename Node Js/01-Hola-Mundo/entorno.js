f0db440674984bddc8d90745be0a700c45d49627
Marina Falcon <falcon.marina.it@gmail.com>
2023-09-22 22:09:09 -0300

    Node 6.2 Prueba para aclarar conceptos

--- Node Js/01-Hola-Mundo/entorno.js
+++ Node Js/01-Hola-Mundo/entorno.js
@@ -1,4 +1,6 @@
 
-let nombre = process.env.NOMBRE;
+let nombre = process.env.NOMBRE || 'Sin nombre';  // or valor por default de la variable de entorno
+let web = process.env.WEB || 'No tengo web';   // las variables de entorno van por convencion en MAYUSCULAS y separadas por guion bajo si fueran dos palabras juntas
 
 console.log('Hola ' + nombre);
+console.log('Mi web es : ' + web);
