f1a6d597c65cb0017775b58ce944ee996e1f6d07
Rosalía <84238521+Ro07-r@users.noreply.github.com>
2023-09-26 21:40:39 -0300

    Agrego carpetas para el backend y creo el servidor express

--- JS/Leccion05/src/app.js
+++ JS/Leccion05/src/app.js
@@ -1,1 +1,11 @@
 // Ponemos el codigo ser servidor
+
+import express from "express";
+
+const app = express();
+
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => res.json({message: "Bienvenidos a mi proyecto"}));
+
+export default app;
