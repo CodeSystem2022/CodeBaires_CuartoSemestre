8ca09e12320db2b63d5fb4f79f3c54e9dbfa2c38
Marina Falcon <falcon.marina.it@gmail.com>
2023-09-27 20:07:49 -0300

    JS - 5.8 parte 2
    Agregando rutas al servidor

--- JS/Leccion05/src/router/outh.router.js
+++ JS/Leccion05/src/router/outh.router.js
@@ -1,9 +1,19 @@
 import { Router } from "express";
-import { signin, signup, signout, profile } from "../controllers/auth.cotroller.js";
+
+//import { signin, signup, signout, profile } from "../controllers/auth.cotroller.js";
+
 const router = Router ();
-router.post("/signin",signin);
+
+/*router.post("/signin",signin);
 router.post("/signin",signup);
 router.post("/signin",signout);
 router.get("/signin",profile);
+*/
+
+router.post("/signin", (req,res) =>res.send ("ingresando"));
+router.post("/signup", (req,res) =>res.send ("resgistrando"));
+router.post("/signout",(req,res) =>res.send ("Cerrando sesion));
+router.get("/profile", (req,res) =>res.send ("Perfil de usuario"));
+
 
 export default router;
