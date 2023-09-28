1ee99e2f5c215cdeeee9ee18357955b3d687dc24
YamilaCarlucci <yamila.carlucci@gmail.com>
2023-09-27 21:13:41 -0300

    Fix archivos js

--- JS/Leccion05/src/router/auth.routers.js
+++ JS/Leccion05/src/router/auth.routers.js
@@ -1,19 +1,16 @@
 import { Router } from "express";
 
-//import { signin, signup, signout, profile } from "../controllers/auth.cotroller.js";
+import { profile, signin, signout, signup } from "../controllers/auth.cotroller.js";
 
 const router = Router ();
 
-/*router.post("/signin",signin);
-router.post("/signin",signup);
-router.post("/signin",signout);
-router.get("/signin",profile);
-*/
+router.post("/signin", signin);
 
-router.post("/signin", (req,res) =>res.send ("ingresando"));
-router.post("/signup", (req,res) =>res.send ("resgistrando"));
-router.post("/signout",(req,res) =>res.send ("Cerrando sesion"));
-router.get("/profile", (req,res) =>res.send ("Perfil de usuario"));
+router.post("/signup", signup);
+
+router.post("/signout", signout);
+
+router.get("/profile", profile);
 
 
 export default router;
