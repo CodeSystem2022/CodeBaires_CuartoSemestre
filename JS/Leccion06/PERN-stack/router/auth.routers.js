a5199a7aa016640e86637df9dfd9dd7831920ddb
YamilaCarlucci <yamila.carlucci@gmail.com>
2023-09-28 18:41:54 -0300

    Fix ubicaciones de archivos

--- JS/Leccion06/PERN-stack/router/auth.routers.js
+++ JS/Leccion06/PERN-stack/router/auth.routers.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import { profile, signin, signout, signup } from "../controllers/auth.controller.js";
+import { profile, signin, signout, signup } from "controllers/auth.controller.js";
 
 const router = Router ();
 
