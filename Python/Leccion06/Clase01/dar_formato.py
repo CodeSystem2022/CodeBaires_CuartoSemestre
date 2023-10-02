bc6c34f15a9a66045d399e1453f5274ed695cd4c
Matias Quispe <111471872+matiasq3@users.noreply.github.com>
2023-10-02 19:46:59 -0300

    Agrego parte 6.3.3 de Python
    Seguimos con el  uso del método format() -> utilizamos place holder - parte 3

--- Python/Leccion06/Clase01/dar_formato.py
+++ Python/Leccion06/Clase01/dar_formato.py
@@ -17,3 +17,13 @@ sueldo = 3000
 
 mensaje = 'Nombre {0} Edad {1} Sueldo {2:.2f}'.format(nombre, edad, sueldo)
 print(mensaje)
+
+#6.3 parte 3
+mensaje = 'Nombre {n} Edad {e} Sueldo {s:.2f}'.format(n=nombre, e=edad , s=sueldo) # Lo que tiene, se lo asignamos a la variable  n
+# print(mensaje)
+
+diccionario = {'nombre':'Ivan', 'edad': 35, 'sueldo':8000.00 }
+mensaje = 'Nombre {dic[nombre]} Edad {dic[edad]} Sueldo {dic[sueldo]}'.format(dic=diccionario)  #Renombramos el diccionario de terminos, con dic accedemos al valor de cada uno
+print(mensaje)
+
+
