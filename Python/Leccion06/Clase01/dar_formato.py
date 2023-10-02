c36cdcfc4f0633ad73cad2e4332fcf5af9e64efa
Matias Quispe <111471872+matiasq3@users.noreply.github.com>
2023-10-02 19:06:13 -0300

    Python_6.3 (parte1)
     Uso del método format() -> utilizamos place holder

--- Python/Leccion06/Clase01/dar_formato.py
+++ Python/Leccion06/Clase01/dar_formato.py
@@ -4,3 +4,16 @@ nombre = 'Ariel'
 edad = 28
 mensaje_con_formato = 'MI nombre es %s y tengo %d años'%(nombre, edad)
 print(mensaje_con_formato)
+
+
+
+
+# 6.3 Uso del método format() -> utilizamos place holder 
+nombre = 'Juan'
+edad = 19
+sueldo = 3000
+#mensaje_con_formato = 'nombre {} edad {} sueldo {:.2f}'.format(nombre, edad, sueldo ) #Marcador de posicion
+#print(mensaje_con_formato)
+
+mensaje = 'Nombre {0} Edad {1} Sueldo {2:.2f}'.format(nombre, edad, sueldo)
+print(mensaje)
