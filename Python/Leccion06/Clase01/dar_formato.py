c9290e1de0e86e284e364e637c9a0e0bd83f30e1
MaiaStrusiat <112667066+MaiaStrusiat@users.noreply.github.com>
2023-10-02 21:48:23 +0200

    Subo practica Python, creando un tupla

--- Python/Leccion06/Clase01/dar_formato.py
+++ Python/Leccion06/Clase01/dar_formato.py
@@ -3,4 +3,8 @@
 nombre = 'Ariel'
 edad = 28
 mensaje_con_formato = 'MI nombre es %s y tengo %d años'%(nombre, edad)
-print(mensaje_con_formato)
+
+#Creamos una tupla
+persona = ('Carla','Gomez',5000.00)
+mensaje_con_formato = 'Hola %s %s . Tu sueldo es %.2f' # % persona #Aqui le pasamos  el objeto tupla
+print(mensaje_con_formato % persona)
