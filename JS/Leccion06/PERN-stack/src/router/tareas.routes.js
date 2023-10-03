c3400fe5f7bd87a32b57a94f31711b6ecb7bce36
Marina Falcon <falcon.marina.it@gmail.com>
2023-10-03 19:31:49 -0300

    subo JS--6.3

--- JS/Leccion06/PERN-stack/src/router/tareas.routes.js
+++ JS/Leccion06/PERN-stack/src/router/tareas.routes.js
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import  Router  from "express-promise-router";
 import { listarTareas, listarTarea, crearTarea, actualizarTarea, eliminarTarea } from "../controllers/tareas.controller.js";
 
 const router = Router();
