b01cb2a4dde8f9999fb22f36d8e902584af98ca8
Marina Falcon <falcon.marina.it@gmail.com>
2023-10-03 03:28:21 -0300

    Update tareas.controller.js

--- JS/Leccion06/PERN-stack/src/controllers/tareas.controller.js
+++ JS/Leccion06/PERN-stack/src/controllers/tareas.controller.js
@@ -1,10 +1,26 @@
+import {pool} from '../db.js';
+
 export const listarTareas = (req, res)=> res.send('obteniendo tareas');
 
 export const listarTarea = (req, res)=> res.send('obteniendo tarea unica');
 
-export const crearTarea = (req, res)=> {
-    console.log(req.body);
-    res.send('Creando tarea');
+export const crearTarea = async(req, res,next)=> {
+    const {titulo, descripcion } = req.body;
+
+    try {
+        throw new Error('algo salio mal');
+        const {rows} = await pool.query('INSERT INTO tareas (titulo, descripcion ) VALUES ($1,$2)', [titulo, descripcion]);
+            console.log(rows);
+            res.send('Creando tarea');
+    } catch (error) {
+        if (error.code == '23505') {
+            return res.send('ya existe una tarea con ese titulo');
+        }
+        console.log(error);
+        next(error);
+    }
+    
+
 }
 
 export const actualizarTarea = (req, res)=> res.send('actualizando tarea unica');
