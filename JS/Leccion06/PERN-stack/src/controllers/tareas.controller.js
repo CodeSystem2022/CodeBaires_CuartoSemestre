ecf04644884e80907a88ac91d00f94a5e1a83842
YamilaCarlucci <yamila.carlucci@gmail.com>
2023-10-02 21:34:02 -0300

    Manejando errores

--- JS/Leccion06/PERN-stack/src/controllers/tareas.controller.js
+++ JS/Leccion06/PERN-stack/src/controllers/tareas.controller.js
@@ -4,12 +4,17 @@ export const listarTareas = (req, res)=> res.send('obteniendo tareas');
 
 export const listarTarea = (req, res)=> res.send('obteniendo tarea unica');
 
-export const crearTarea = (req, res)=> {
+export const crearTarea = async(req, res)=> {
     const {titulo, descripcion} = req.body;
+    
+    try { const {rows} = await pool.query('INSERT INTO tareas (titulo, descripcion) VALUES ($1, $2)', [titulo, descripcion])
+    console.log(rows);
     res.send('Creando tarea');
 
-    const {rows} = pool.query('INSERT INTO tareas (titulo, descripcion) VALUES ($1, $2)', [titulo, descripcion])
-    console.log(rows);
+    } catch (error) { 
+        console.log("Algo salió mal")
+    }
+    
 }
 
 export const actualizarTarea = (req, res)=> res.send('actualizando tarea unica');
