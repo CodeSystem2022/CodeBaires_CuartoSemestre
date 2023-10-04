b7b8285374e38e4bc23ff95be858942b643558d2
YamilaCarlucci <yamila.carlucci@gmail.com>
2023-10-04 14:59:26 -0300

    Fix en consultas

--- JS/Leccion06/PERN-stack/src/controllers/tareas.controller.js
+++ JS/Leccion06/PERN-stack/src/controllers/tareas.controller.js
@@ -1,25 +1,26 @@
 import {pool} from '../db.js';
 
-export const listarTareas = async(req, res)=> {
+export const listarTareas = async (req, res)=> {
     const resultado = await pool.query('SELECT * FROM tareas');
     console.log(resultado);
     return res.json(resultado.rows);
 }
 
-export const listarTarea = async(req,res) => {
-    const resultado = await pool.query('SELECT = FROM tareas    WHERE id =$1', [req.params.id]);
-    if (resultado.rowCout === 0) {
+export const listarTarea = async (req,res) => {
+    const resultado = await pool.query('SELECT * FROM tareas WHERE id = $1', [req.params.id]);
+    if (resultado.rowCount === 0) {
         return res.status(404).json({
             message: 'La tarea no existe'
         });
     }
+    return res.json(resultado.rows[0]);
 }
 
-export const crearTarea = async(req, res,next)=> {
+export const crearTarea = async (req, res,next)=> {
     const {titulo, descripcion } = req.body;
 
     try {
-        const result = await pool.query('INSERT INTO tareas (titulo, descripcion ) VALUES ($1,$2) RETURNING', [titulo, descripcion]);
+        const result = await pool.query('INSERT INTO tareas (titulo, descripcion) VALUES ($1, $2) RETURNING *', [titulo, descripcion]);
         res.json(result.rows[0]);
         console.log(result.rows[0]);
     } catch (error) {
