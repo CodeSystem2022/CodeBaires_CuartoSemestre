9ffc2ab9c37220eb2114dc7caeee7e8f24fec219
Maria Rodriguez <marialrodriguez027@gmail.com>
2023-10-04 12:39:46 -0300

    subo js-6.5

--- JS/Leccion06/PERN-stack/src/controllers/tareas.controller.js
+++ JS/Leccion06/PERN-stack/src/controllers/tareas.controller.js
@@ -38,4 +38,14 @@ export const crearTarea = async(req, res,next)=> {
 
 export const actualizarTarea = (req, res)=> res.send('actualizando tarea unica');
 
-export const eliminarTarea = (req, res)=> res.send('eliminando tarea unica ');
+export const eliminarTarea = async (req, res)=> {
+    const resultado = await pool.query('DELETE FROM tareas WHERE id = $1',[req.params.id]);
+    
+    if (resultado.rowCount === 0) {
+        return res.status(404).json({
+            message: 'No existe una tarea con ese id'
+        });
+    }
+
+    return res.sendStatus(204);
+}
