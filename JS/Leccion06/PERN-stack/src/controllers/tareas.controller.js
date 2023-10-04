0bdb393999591adc8b42e118feb6125897ca78ef
YamilaCarlucci <yamila.carlucci@gmail.com>
2023-10-04 15:24:42 -0300

    Agrego actualizarTarea

--- JS/Leccion06/PERN-stack/src/controllers/tareas.controller.js
+++ JS/Leccion06/PERN-stack/src/controllers/tareas.controller.js
@@ -34,7 +34,17 @@ export const crearTarea = async (req, res,next)=> {
     }
 }
 
-export const actualizarTarea = (req, res)=> res.send('Actualizando tarea unica');
+export const actualizarTarea = async (req, res)=> {
+    const {titulo, descripcion } = req.body;
+    const id = req.params.id;
+    const resultado = await pool.query('UPDATE tareas SET titulo = $1, descripcion = $2 WHERE id = $3 RETURNING *',[titulo, descripcion, id]);
+    if (resultado.rowCount === 0) {
+        return res.status(404).json({
+            message: 'No existe una tarea con ese id'
+        });
+    }
+    return res.json(resultado.rows[0]);
+}
 
 export const eliminarTarea = async (req, res) => {
     const resultado = await pool.query('DELETE FROM tareas WHERE id = $1', [req.params.id]);
