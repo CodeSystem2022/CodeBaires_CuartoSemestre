45d58e043b9dd7ddfd350cdbaebec369956a0db8
MaiaStrusiat <112667066+MaiaStrusiat@users.noreply.github.com>
2023-10-08 02:41:53 +0200

    Continuamos con el constructor

---
+++ Java/Leccion09/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
@@ -0,0 +1,18 @@
+package utn.tienda_libros;
+
+import org.springframework.boot.ConfigurableBootstrapContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class TiendaLibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaLibrosApplication.class, args);
+	}
+
+	ConfigurableApplicationContext contextoSpring=
+			new SpringApplicationBuilder(TiendaLibrosApplication.class)
+}
