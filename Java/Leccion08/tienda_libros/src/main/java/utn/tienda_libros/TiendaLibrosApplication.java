9c152582d3aba4281c7500d943141f8dd7662dc1
YamilaCarlucci <yamila.carlucci@gmail.com>
2023-10-09 13:33:53 -0300

    Fix Java 8.2

--- Java/Leccion08/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
+++ Java/Leccion08/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
@@ -1,30 +1,34 @@
 package utn.tienda_libros;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
+import utn.tienda_libros.vista.LibroFrom;
 
-import java.EventQueue;
+import java.awt.*;
+
 
 @SpringBootApplication
-public class TiendaLibrosApplication 
-
+public class TiendaLibrosApplication {
 	public static void main(String[] args) {
-		
-	}
+
 		ConfigurableApplicationContext contextoSpring =
-				 new SpringApplicationBuilder(TiendaLibrosApplication.class)
-                      .headlees(false)
-					  .web(WebAPlicationType.NONE)
-					  .rum(args);
-       
-//Ejecutamos el codigo para el formulario
-EventQueue.invokeLater(( )-> { //Metodo Lambda
-	//obtenemos el objeto from a traves del spring
-	LibroFrom libroFrom = contextoSpring.getBean(LibroFrom.class);
-	LibroFrom.setVisible(true);
-    });	
+				new SpringApplicationBuilder(TiendaLibrosApplication.class)
+						.headless(false)
+						.web(WebApplicationType.NONE)
+						.run(args);
+
+		//Ejecutamos el codigo para el formulario
+		EventQueue.invokeLater(() -> { //Método Lambda
+			// Obtenemos el objeto from a traves del spring
+			LibroFrom libroFrom = contextoSpring.getBean(LibroFrom.class);
+			libroFrom.setVisible(true);
+		});
+	}
+}
+
+
 
 
 
