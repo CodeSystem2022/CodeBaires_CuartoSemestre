7561f734bee5f6935705efbb5632c831eac2a9bb
Emmanueltoro28 <111504496+Emmanueltoro28@users.noreply.github.com>
2023-10-10 20:36:57 -0300

    subo JS parte 1

--- JS/Leccion07/PERN-stack/src/controllers/auth.controller.js
+++ JS/Leccion07/PERN-stack/src/controllers/auth.controller.js
@@ -1,6 +1,13 @@
+import {pool} from "../db.js"
+
 export const signin = (req, res)=> res.send('ingresando');
 
-export const signup = (req, res)=> res.send('registrando');
+export const signup = async(req, res) => {
+    const {name , email, password} = req.body;
+    res.send("Registrando");
+   const result = await pool.query("INSERT INTO usuarios (name, email, password) VALUES ($1, $2, $3", [name, email, password])
+   console.log(result);
+};
 
 export const signout = (req, res)=> res.send('Cerrando sesion');
 
