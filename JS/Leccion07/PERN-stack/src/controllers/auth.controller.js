e3f1a8ff217aa3505369e0065125c54a329a6ea0
Emmanueltoro28 <111504496+Emmanueltoro28@users.noreply.github.com>
2023-10-10 21:46:53 -0300

    subo parte 3

--- JS/Leccion07/PERN-stack/src/controllers/auth.controller.js
+++ JS/Leccion07/PERN-stack/src/controllers/auth.controller.js
@@ -4,16 +4,17 @@ export const signin = (req, res)=> res.send('ingresando');
 
 export const signup = async(req, res) => {
     const {name , email, password} = req.body;
-    res.send("Registrando");
 
 try {
-    const result = await pool.query("INSERT INTO usuarios (name, email, password) VALUES ($1, $2, $3", [name, email, password])
-    console.log(result);
-   return res.json{result.rows[0]};
+    const result = await pool.query("INSERT INTO usuarios (name, email, password) VALUES ($1, $2, $3 RETURNING *", [name, email, password])
 
-} catch (error) {
-    
-}
+   console.log(result);
+   return res.json(result.rows[0]);
+ } catch (error) {
+  if(error.code === "23505"){
+      return res.status(4000).json({message: " El correo ya esta registrado"});
+   }  
+ }
    
 };
 
