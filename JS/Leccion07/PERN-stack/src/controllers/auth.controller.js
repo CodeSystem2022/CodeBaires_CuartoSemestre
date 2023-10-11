1c3fb961fd83082935b82e0f223d7f8354d00d07
YamilaCarlucci <yamila.carlucci@gmail.com>
2023-10-11 11:07:11 -0300

    7.4 Cookies -> Guardar el token en una cookie

--- JS/Leccion07/PERN-stack/src/controllers/auth.controller.js
+++ JS/Leccion07/PERN-stack/src/controllers/auth.controller.js
@@ -17,10 +17,12 @@ export const signup = async (req, res) => {
     
     const token = await createAccessToken({id: result.rows[0].id});
     console.log(result);
-    // return res.json(result.rows[0]);
-    return res.json({
-      token: token,
+    res.cookie("token", token, {
+      httpOnly: true,
+      sameSite: "none",
+      maxAge: 60 * 60 * 24 * 1000,//1 dia
     });
+    return res.json(result.rows[0]);
   } catch (error) {
     if(error.code === "23505"){
       return res.status(400).json({message: "El correo ya está registrado"});
