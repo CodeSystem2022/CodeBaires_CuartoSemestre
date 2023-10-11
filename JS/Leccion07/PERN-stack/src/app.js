b1976db65ca548795ea3c71cbba5d41474d04684
YamilaCarlucci <yamila.carlucci@gmail.com>
2023-10-11 13:54:03 -0300

    7.5 Creación de un middleware -> auth.middleware.js (Parte 3)

--- JS/Leccion07/PERN-stack/src/app.js
+++ JS/Leccion07/PERN-stack/src/app.js
@@ -3,18 +3,19 @@ import express from "express";
 import morgan from "morgan";
 import tareasRoutes from "./router/tareas.routes.js";
 import authRoutes from "./router/auth.routes.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 //Middlewares
 app.use(morgan("dev"));
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => res.json({message: "Bienvenidos a mi proyecto"}));
 app.use('/api',tareasRoutes);
 app.use('/api',authRoutes);
-
 
 
 //Manejando errores
