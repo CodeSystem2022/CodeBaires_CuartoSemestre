010e022b3eb7cc6dfb9ef768bb2de579b306e3d6
Emmanueltoro28 <111504496+Emmanueltoro28@users.noreply.github.com>
2023-10-14 23:18:51 -0300

    subo js-8.1

--- JS/Leccion08/PERN-stack/src/controllers/auth.controller.js
+++ JS/Leccion08/PERN-stack/src/controllers/auth.controller.js
@@ -2,7 +2,34 @@ import { pool } from "../db.js";
 import bcrypt from "bcrypt";
 import {createAccessToken} from "../libs/jwt.js";
 
-export const signin = (req, res) => res.send("Ingresando");
+export const signin = (req, res) => {
+  const (email, password) = req.body;
+
+  const result = await pool.query("SELECT * FROM usuarios WHERE email = $1", [email])
+
+  if(result.rowCount === 0){
+    return res.status(400).json({message: "El correo no esta registrado"});
+  }
+
+  const validPassword = await bcrypt.compare(password, result.rows[0].password);
+  
+  if (!validPassword){
+       return res.status(400).json({message: "La contraseña es incorrecta"});
+
+  }
+  
+  const token = await createAccessToken({id: result.rows[0].id});
+    console.log(result);
+    res.cookie("token", token, {
+      httpOnly: true,
+      sameSite: "none",
+      maxAge: 60 * 60 * 24 * 1000,}) //1 dia
+    return res.json(result.rows[0]);
+  
+    }
+
+}
+
 
 export const signup = async (req, res) => {
   const { name, email, password } = req.body;
