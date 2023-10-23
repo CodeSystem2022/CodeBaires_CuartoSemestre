6c46584b68fb64c6b00668332f2ac898348f6cdf
MaiaStrusiat <112667066+MaiaStrusiat@users.noreply.github.com>
2023-10-23 23:28:18 +0200

    Agrego Eliminar Libro -> Botón eliminar

--- Java/Leccion10/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroFrom.java
+++ Java/Leccion10/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroFrom.java
@@ -41,6 +41,7 @@ public class LibroFrom extends JFrame {
             }
         });
         modificarButton.addActionListener(e -> modificarLibro());
+        eliminarButton.addActionListener(e ->eliminarLibro());
     }
 
     private void iniciarForma(){
@@ -114,6 +115,23 @@ public class LibroFrom extends JFrame {
             mostrarMensaje("Se modifico el libro...");
             limpiarFormulario();
             listarLibros();
+        }
+    }
+
+    private void eliminarLibro(){
+        var renglon = tablaLibros.getSelectedRow();
+        if(renglon != -1){
+            String idLibro =
+                    tablaLibros.getModel().getValueAt(renglon, 0).toString();
+            var libro = new Libro();
+            libro.setIdLibro(Integer.parseInt(idLibro));
+            libroServicio.eliminarLibro(libro);
+            mostrarMensaje("Libro "+idLibro+" ELIMINADO");
+            limpiarFormulario();
+            listarLibros();
+        }
+        else{
+            mostrarMensaje("No se ha seleccionado ningun libro de la tabla a eliminar");
         }
     }
 
