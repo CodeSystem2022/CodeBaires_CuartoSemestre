b65b67731a4bd5e898c8e1b594887a5e7e5f7e0e
Emmanueltoro28 <111504496+Emmanueltoro28@users.noreply.github.com>
2023-10-31 00:41:03 -0300

    subo parte 3 y 4

--- JS/Leccion09/PERN-stack/frontend/src/main.jsx
+++ JS/Leccion09/PERN-stack/frontend/src/main.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from '../src/context/AuthContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider> 
     </BrowserRouter>
   </React.StrictMode>,
 )
