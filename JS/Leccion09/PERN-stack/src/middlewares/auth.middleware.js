dbdbd0c19b2cd982cbe41bf0d1b22ef4c6bc4510
YamilaCarlucci <yamila.carlucci@gmail.com>
2023-10-31 20:43:46 -0300

    Fix JS

--- JS/Leccion09/PERN-stack/src/middlewares/auth.middleware.js
+++ JS/Leccion09/PERN-stack/src/middlewares/auth.middleware.js
@@ -2,18 +2,18 @@ import jwt from "jsonwebtoken";
 
 export const isAuth = (req, res, next) => {
     const token = req.cookies.token;
-
     if (!token) {
         return res.status(401).json({
-            message: 'No estas autorizado'
+            message:"No estás autorizado"
         });
     }
-   jwt.verify(token, "xyz123", (err, decoded) => {
-        if (err) 
+    jwt.verify(token, "xyz123", (err, decoded) => {
+        if(err) {
             return res.status(401).json({
-                message: 'No estas autorizado'
+                message:"No estás autorizado"
             });
-        req.usuarioId = decoded.id;
-        next();
+    }
+    req.usuarioId = decoded.id;
+    next();
     });
 };
