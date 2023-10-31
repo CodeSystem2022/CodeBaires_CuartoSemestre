af80b66de2cfec2ced97b142b85243904e9674c3
Emmanueltoro28 <111504496+Emmanueltoro28@users.noreply.github.com>
2023-10-31 00:07:15 -0300

    subo parte 2

--- JS/Leccion09/PERN-stack/frontend/src/pages/RegisterPage.jsx
+++ JS/Leccion09/PERN-stack/frontend/src/pages/RegisterPage.jsx
@@ -1,55 +1,62 @@
-import { Button, Card, Input } from "../components/ui";
-import { useForm } from "react-hook-form";
+import { Button, Card, Container, Input, Label } from "../components/ui";
+import  { useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
 
 function RegisterPage() {
-
-  const { register, handleSubmit, formState: { errors } } = useForm();
-
-  const onSubmit = handleSubmit((data) => {
-    console.log(data);
-    const response = await fetch("http://localhost:3001/api/auth/register", {
-      method: "POST",
-      body:JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json",
-      }});
-      const json = await response.json();
-      console.log(json);
-
-    });
   
+  const { register, handleSubmit, formState: {errors} } = useForm();
+
+  const {signup, errors: setUserErrors } = useAuth();
+  const navigate = useNavigate();
+  const onSubmit = handleSubmit(async(data) => { 
+    const user = await signup(data);
+    if(user){
+      navigate("/tareas");
+    }
+  });
+
 
   return (
-    <div className="h-[calc(100vh-64px)] flex items-center justify-center">
+    <Container className="h-[calc(100vh-10rem)] flex items-center justify-center">
       <Card>
-        <h3 className="text-2xl font-bold">Registro</h3>
+        {setUserErrors &&
+          setUserErrors.map((error) => (
+            <p className="bg-red-500 text-white p-2">{error}</p>
+          ))}
+        <h3 className="text-4xl font-bold my-2">Registro</h3>
         <form onSubmit={onSubmit}>
-          <Input placeholder="Ingrese su nombre "
-            {...register("name", { required: true })}></Input>
+          <Label htmlFor="name">Nombre</Label>
+          <Input placeholder="Ingrese su nombre"
+          {...register("name", {required:true})}></Input>
 
-          {
-            errors.name && <p className="text-red-500">Este campo es requerido</p>
-          }
+        {
+          errors.name && <p className="text-red-500">Este campo es requerido</p>
+        }
+          <Label htmlFor="email">Email</Label>
+          <Input type="email" placeholder="Ingrese su email"
+          {...register("email", {required:true})}></Input>
 
-          <Input type="email" placeholder="Ingrese su email "
-            {...register("email", { required: true })}></Input>
+        {
+          errors.email && <p className="text-red-500">Este campo es requerido</p>
+        }
+          <Label htmlFor="password">Contraseña</Label>
+          <Input type="password" placeholder="Ingrese su contraseña"
+          {...register("password", {required:true})}></Input>
 
-          {
-            errors.email && <p className="text-red-500">Este campo es requerido</p>
-          }
-
-          <Input type="password" placeholder="Ingrese su contraseña "
-            {...register("password", { required: true })}></Input>
-
-          {
-            errors.password && <p className="text-red-500">Este campo es requerido</p>
-          }
-
+        {
+          errors.password && <p className="text-red-500">Este campo es requerido</p>
+        }
           <Button>Registrarse</Button>
         </form>
+        <div className=" flex justify-between my-4">
+          <p className="mr-4">¿Ya tienes cuenta?</p>
+          <Link to="/login">Inciar Sesión</Link>
+        </div>
       </Card>
-    </div>
+    </Container>
   );
 }
 
-export default RegisterPage
+export default RegisterPage;
