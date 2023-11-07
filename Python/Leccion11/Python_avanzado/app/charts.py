a46f92b7f748b8155e88dcc9bfdc1ef336faa3b7
YamilaCarlucci <yamila.carlucci@gmail.com>
2023-11-07 20:30:49 -0300

    11.4

--- Python/Leccion11/Python_avanzado/app/charts.py
+++ Python/Leccion11/Python_avanzado/app/charts.py
@@ -1,18 +1,25 @@
 import matplotlib.pyplot as plt
 
+
 def generate_bar_chart(name, labels, values):
-  fig, ax = plt.subplots()
-  ax.bar(labels, values)
-  plt.show()
+    fig, ax = plt.subplots()
+    ax.bar(labels, values)
+    # plt.show()
+    plt.savefig('pie.png')
+    plt.close()
+
 
 def generate_pie_chart(labels, values):
-  fig, ax = plt.subplots()
-  ax.pie(values, labels=labels)
-  ax.axis('equal')
-  plt.show()
+    fig, ax = plt.subplots()
+    ax.pie(values, labels=labels)
+    ax.axis('equal')
+    # plt.show()
+    plt.savefig('pie.png')
+    plt.close()
+
 
 if __name__ == '__main__':
-  labels = ['a', 'b', 'c']
-  values = [10, 40, 800]
-  # generate_bar_chart(labels, values)
-  generate_pie_chart(labels, values)
+    labels = ['a', 'b', 'c']
+    values = [10, 40, 800]
+    # generate_bar_chart(labels, values)
+    generate_pie_chart(labels, values)
