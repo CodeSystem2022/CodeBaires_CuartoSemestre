ce1f031bcd926bed321bce98df529bd4127d889f
YamilaCarlucci <yamila.carlucci@gmail.com>
2023-11-08 21:45:21 -0300

    NODE JS 13.2

--- Node Js/02-Fundamentos_node/modulos/fs.js
+++ Node Js/02-Fundamentos_node/modulos/fs.js
@@ -1,1 +1,11 @@
 const fs = require('fs');
+
+//Primero leemos el archivo.txt
+function leer(ruta, cb){
+    fs.readFile(ruta, (err, data) => {
+        console.log(data);
+    });
+}
+
+leer(`${__dirname}/archivo.txt`, console.log); //Sintaxis ES6
+    
