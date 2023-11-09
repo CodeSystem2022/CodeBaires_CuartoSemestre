2989e0a2641e43536395b50a423f4e0a0b5d190d
YamilaCarlucci <yamila.carlucci@gmail.com>
2023-11-08 21:52:28 -0300

    NODE JS 13.3

--- Node Js/02-Fundamentos_node/modulos/fs.js
+++ Node Js/02-Fundamentos_node/modulos/fs.js
@@ -3,9 +3,21 @@ const fs = require('fs');
 //Primero leemos el archivo.txt
 function leer(ruta, cb){
     fs.readFile(ruta, (err, data) => {
-        console.log(data);
+        cb(data.toString());
     });
 }
 
+
+//Segundo escribimos en el archivo.txt
+function escribir(ruta, contenido,cb){
+    fs.writeFile(ruta, contenido, function(err) {
+        if (err) {
+            console.log("No se pudo escribir", err);
+        } else {
+            console.log("Se ha escrito correctamente");
+        }
+     });
+}
+
+//escribir(`${__dirname}/archivo1.txt`, 'Reescribimos el archivo', console.log);
 leer(`${__dirname}/archivo.txt`, console.log); //Sintaxis ES6
-    
